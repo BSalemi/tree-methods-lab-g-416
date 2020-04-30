79ca332146edb4fb22d78ccca265e60f20ef40ee
Brian Salemi <briandsalemi@gmail.com>
2020-04-30 14:22:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ function inOrder(currentNode){
   }
 }
 
+// function findOrAdd(currentNode, newNode){
+//   if(currentNode.data == newNode.data){
+//     return true
+//   };
+//     if( newNode.data < currentNode.data){
+//       if(currentNode.left){
+//         return findOrAdd(currentNode.left, newNode)
+//       } else {
+//         return currentNode.left = newNode
+//       }
+//     }
+// }
+
 function findOrAdd(currentNode, newNode){
   if(currentNode.data == newNode.data){
     return true
-  };
-    if( newNode.data < currentNode.data){
-      if(currentNode.left){
-        return findOrAdd(currentNode.left, newNode)
-      } else {
-        return currentNode.left = newNode
-      }
+  }
+  if(newNode.data < currentNode.data){
+    if(currentNode.left){
+      return findOrAdd(currentNode.left, newNode)
+    } else {
+      return currentNode.left = newNode
     }
+  }
 }
